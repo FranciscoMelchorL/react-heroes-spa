8df0d351268d30f0f002946b6c0aa7dc3d979995
FranciscoMelchorL <melf991203@gs.utm.mx>
2025-02-08 21:35:20 -0600

    docs

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './styles.css';
 import { HeroesApp } from './HeroesApp';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/react-heroes-spa'>
+    <HashRouter>
       <HeroesApp />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
