312b0e4bcb3a60579fe22ace5ff8849cc413a13b
FranciscoMelchorL <melf991203@gs.utm.mx>
2025-02-08 22:15:21 -0600

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { HeroesApp } from './HeroesApp';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <HeroesApp />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
